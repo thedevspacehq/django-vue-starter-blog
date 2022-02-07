20de1e96e617beeff393a9b48887b6aca5e29ecf
Eric Hu <huericnan@gmail.com>
2022-02-07 15:48:52 -0500

    initial commit

---
+++ frontend/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createProvider } from './vue-apollo'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  apolloProvider: createProvider(),
+  render: h => h(App)
+}).$mount('#app')
