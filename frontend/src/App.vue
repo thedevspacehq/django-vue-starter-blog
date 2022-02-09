6b494b2267b524d9ce5751962c1b130d442f1109
Eric Hu <huericnan@gmail.com>
2022-02-09 12:50:31 -0500

    add vue router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h2 class="line-through">Test TailwindCSS</h2>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
